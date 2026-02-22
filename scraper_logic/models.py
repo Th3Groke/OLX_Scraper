7df9ef10f6fd522290c8e1ad219b1657e1ccd79f
Adam Dyrda <a.dyrda102@gmail.com>
2026-02-22 16:14:00 +0100

    changes of the class fields to match the databse structure changes

--- scraper_logic/models.py
+++ scraper_logic/models.py
@@ -1,13 +1,15 @@
 class Listing:
     title = ""
     price = ""
+    price_int = None
     location = ""
     date = ""
     link = ""
 
-    def __init__(self, title, price, location, date, link):
+    def __init__(self, title, price, price_int, location, date, link):
         self.title = title
         self.price = price
+        self.price_int = price_int
         self.location = location
         self.date = date
         self.link = link
